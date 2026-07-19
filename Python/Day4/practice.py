4b2a3da1b57e8bb80c01de7bccb528c68de40110
MG-Seo17 <a01026147538@gmail.com>
2026-07-19 21:43:30 +0900

    Update practice.py

--- Python/Day4/practice.py
+++ Python/Day4/practice.py
@@ -1,8 +1,19 @@
 </python>
-
+#if 구문연습
 if a % 2 == 0:
 ...     print('Even')
 else:
 ...     print('Odd')
 ...
 Odd   #if 연습문 
+-------------------------------------------
+#if - elif 구문 연습
+name = "Seo Myeonggon"
+>>> if name == "Seo Myeonggon":
+...     print("Hello. Myeonggon!")
+... elif name == "Kim Youngsu":
+...     print("Hello. Youngsu")
+... else:
+...     print("Who are you?")
+...
+Hello. Myeonggon!
