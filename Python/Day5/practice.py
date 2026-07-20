44888a853b3b2e03613c17265c43d0885d2164b4
MG-Seo17 <a01026147538@gmail.com>
2026-07-20 22:41:43 +0900

    Update practice.py

--- Python/Day5/practice.py
+++ Python/Day5/practice.py
@@ -1,1 +1,13 @@
+</> python
 
+bool(0.0)
+False  #0.0 부울값이 0이라는 것을 확인
+
+ a = 0
+>>> if a:  #a가 0이므로 False이다 따라서 else식을 실행
+...     print("a is not Zero")
+... else:
+...     print("a is Zero")
+...
+a is Zero
+-------------------------------------------------------------
