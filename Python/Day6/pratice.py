505d065cc72255dbd0f6965c9b1dcea486812361
MG-Seo17 <a01026147538@gmail.com>
2026-07-26 23:22:15 +0900

    Update pratice.py

--- Python/Day6/pratice.py
+++ Python/Day6/pratice.py
@@ -1,20 +1,20 @@
 </> python
 # 뺼셈, 곱하기 함수 만들기
- def sub(a, b) :
-... return a-b
-...
-  File "<python-input-2>", line 2
-    return a-b
-    ^^^^^^
-IndentationError: expected an indented block after function definition on line 1
 >>> def sub(a, b) :
 ...     return a-b
 ...
 >>> sub(3,4)
 -1
+
 >>> def multi(c, d):
 ...     return c*d
 ...
 >>> multi(5, 7)
 35
+
+def intro(name):
+...     return "Hi! "+name
+...
+>>> intro("명곤")
+'Hi! 명곤'
 ----------------------------------------------------------------------------------------------
